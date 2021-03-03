fe90bba48e38235e7283f15b60e57b2cdd2cd08c
cricard7 <chad_ricard@hotmail.com>
2021-03-02 22:20:31 -0600

    initial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="large-12 medium-12 small-12 cell">
+      <label>File
+        <input type="file" id="file" ref="file" v-on:change="handleFileUpload($event)"/>
+      </label>
+        <button v-on:click="submitFile()">Submit</button>
+    </div>
+
+
+  
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+//https://serversideup.net/uploading-files-vuejs-axios/
+
+import papaparse from 'papaparse'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
+  },
+  data: function(){
+    return{
+      file: ''
+    }
+  },
+  methods:{
+    handleFileUpload(e){
+     
+      this.file = this.$refs.file.files[0];
+
+      console.log(this.file)
+      console.log(e.target.files[0])
+
+      //https://edwardize.blogspot.com/2018/04/parsinf-csv-file-import-javascript-vuejs.html
+      //https://www.joyofdata.de/blog/parsing-local-csv-file-with-javascript-papa-parse/
+
+      //have to check the browswer has a FileReader
+      if(window.FileReader){
+        var reader = new FileReader();
+        reader.readAsText(e.target.files[0]);
+        // Handle errors load
+        reader.onload = function(event) {
+          var csv = event.target.result;
+          
+          papaparse.parse(csv,{
+            header: true,
+            dynamicTyping: true,
+            complete: function(results) {
+              
+              console.log(results.data)
+           } 
+          })
+          
+        };
+        reader.onerror = function(evt) {
+          if(evt.target.error.name == "NotReadableError") {
+            alert("Canno't read file !");
+          }
+        };
+
+      }
+
+      else {
+        alert('FileReader are not supported in this browser.');
+      }
+     
+    }
+  },
+
+  mounted: function(){
+
+
+
   }
 }
 </script>
